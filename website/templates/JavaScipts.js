3b54930bf8bc089094fd3c60ae107eb1ac2c1125
Adrian <adrian90909@ont.pl>
2022-12-01 18:53:10 +0100

    added style - button's (done!) / slider (not work :/ )

--- website/templates/JavaScipts.js
+++ website/templates/JavaScipts.js
@@ -1,8 +1,9 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
+
+const sliderValue = document.querySelector("span");
+const inputSlider = document.querySelector("input");
+inputSlider.oninput = (()=>{
+    let value = inputSlider.value;
+    slider.Value.textContent = value;
+    sliderValue.style.left = (value/2) + "%";
+});
