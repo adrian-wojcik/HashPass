5c5c8d4c7eb3f06fa0f196353f76209eec3be43b
Adrian <adrian90909@ont.pl>
2022-12-03 11:34:32 +0100

    slider range works - problem solved (JavaScript file typo)

--- website/templates/JavaScipts.js
+++
@@ -1,9 +0,0 @@
-
-
-const sliderValue = document.querySelector("span");
-const inputSlider = document.querySelector("input");
-inputSlider.oninput = (()=>{
-    let value = inputSlider.value;
-    slider.Value.textContent = value;
-    sliderValue.style.left = (value/2) + "%";
-});
